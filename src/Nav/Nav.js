d935923ad27e6d969f20a0591d5364fbe33c16c6
Ketan Totlani <itsketant@gmail.com>
2021-08-03 21:35:14 +0530

    main

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -10,16 +10,16 @@ import {
 export default class Nav extends Component {
     render() {
         return (
-            <nav className="container navbar navbar-expand-lg navbar-light pt-4 fixed-top navclass">
-                <div className=" container-fluid">
+            <nav className="container navbar navbar-expand-lg  navbar-light pt-4 fixed-top navclass">
+                <div className=" container-fluid ">
                     <a className="navbar-brand me-auto"  href="#">
                     <img src={Logo} width="100em" alt="" />
                     </a>
                     <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div className="navbar-collapse justify-content-end collapse show" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
+                    <div className=" collapse navbar-collapse  justify-content-end " id="navbarNavAltMarkup">
+                        <div className="navbar-nav ">
                             <a className="nav-link mx-1 linkscss" href="#mens">MEN</a>
                             <a className="nav-link mx-1 linkscss" href="#womens">WOMEN</a>
                             <a className="nav-link mx-1 linkscss" href="#newarrivals">NEW ARRIVALS</a>
